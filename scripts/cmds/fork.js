ee540258595a3b7e63d3b41755cdc7a71bcccbf7
NeoKEX <209222882+NeoKEX@users.noreply.github.com>
2026-02-23 13:08:46 +0000

    Update

---
+++ scripts/cmds/fork.js
@@ -0,0 +1,21 @@
+module.exports = {
+  config: {
+    name: "fork",
+    aliases: ["repo", "source"],
+    version: "1.0",
+    author: "NeoKEX",
+    countDown: 3,
+    role: 0,
+    longDescription: "Returns the link to the official, updated fork of the bot's repository.",
+    category: "system",
+    guide: { en: "{pn}" }
+  },
+
+  onStart: async function({ message }) {
+    const text = "✓ | Here is the updated fork:\n\nhttps://github.com/NeoKEX/Goatbot-updated.git\n\n" +
+                 "Changes:\n1. No Google Credentials needed\n2. Enhanced overall performance\n3. Now using neokex-fca(v4.5.4)\n\nNB: If you want to use neokex-fca please install by typing: npm i neokex-fca@latest\n\n" +
+                 "Keep supporting^_^";
+    
+    message.reply(text);
+  }
+};
